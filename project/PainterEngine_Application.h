426a5e125daf407f4646dfa3a475fae0fe9c641f
zhuyunfan <929089902@qq.com>
2021-06-21 11:03:36 +0800

    去除weak在.h

--- project/PainterEngine_Application.h
+++ project/PainterEngine_Application.h
@@ -17,10 +17,10 @@ typedef struct
 
 extern PX_Application App;
 
-RT_WEAK px_bool PX_ApplicationInitialize(PX_Application *pApp,px_int screen_width,px_int screen_height);
-RT_WEAK px_void PX_ApplicationUpdate(PX_Application *pApp,px_dword elpased);
-RT_WEAK px_void PX_ApplicationRender(PX_Application *pApp,px_dword elpased);
-RT_WEAK px_void PX_ApplicationPostEvent(PX_Application *pApp,PX_Object_Event e);
+px_bool PX_ApplicationInitialize(PX_Application *pApp,px_int screen_width,px_int screen_height);
+px_void PX_ApplicationUpdate(PX_Application *pApp,px_dword elpased);
+px_void PX_ApplicationRender(PX_Application *pApp,px_dword elpased);
+px_void PX_ApplicationPostEvent(PX_Application *pApp,PX_Object_Event e);
 
 void init_px_thread(void);
 
